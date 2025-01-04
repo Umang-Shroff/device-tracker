5e0dd85a2516028b633968b6a47fabc31f34530d
Umang <soyumang55@gmail.com>
2025-01-05 00:06:26 +0530

    socketio setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const socketio = require("socketio");
+const http = require("http");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 const app = express();
 const PORT = process.env.PORT || 5000
