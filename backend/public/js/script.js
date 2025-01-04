bac93a3bddb2b4c9c7d056486bfee9d2dd7624bb
Umang <soyumang55@gmail.com>
2025-01-05 00:52:56 +0530

    map api setup

--- backend/public/js/script.js
+++ backend/public/js/script.js
@@ -12,3 +12,10 @@ if(navigator.geolocation){
         maximumAge: 0,
     })
 }
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Full Map"
+}).addTo(map)
+
